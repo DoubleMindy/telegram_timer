6469d30024aa800f355c2ab0626cc81aab939a0b
DoubleMindy <doublemind21@gmail.com>
2020-07-06 15:15:22 +0300

    В notify_progressbar добавлен третий аргумент

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from pytimeparse import parse
 
 TOKEN=os.getenv("TOKEN")
 CHAT_ID=os.getenv("CHAT_ID")
-is_first_iteration = True
-time = 0
 
 def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
     iteration = min(total, iteration)
@@ -16,12 +14,8 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='
     pbar = fill * filled_length + zfill * (length - filled_length)
     return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
 
-def notify_progress(secs_left, message_id):
-	global is_first_iteration, time
-	if is_first_iteration:
-		is_first_iteration = False
-		time = secs_left
-	bot.update_message(CHAT_ID, message_id, render_progressbar(time, time-secs_left))
+def notify_progress(secs_left, message_id, sum_time):
+	bot.update_message(CHAT_ID, message_id, render_progressbar(sum_time, sum_time-secs_left))
 
 def notify():
 	bot.send_message(CHAT_ID, "Время вышло!")
@@ -30,7 +24,7 @@ def say_hello(text):
 	parsed_time = parse(text)
 	bot.create_timer(parsed_time, notify)
 	message_id = bot.send_message(CHAT_ID, "Таймер запущен на {} секунд".format(parsed_time))
-	bot.create_countdown(parsed_time, notify_progress, message_id = message_id)
+	bot.create_countdown(parsed_time, notify_progress, message_id = message_id, sum_time = parsed_time)
 
 bot = ptbot.Bot(TOKEN)
 bot.send_message(CHAT_ID, "Привет! На сколько запустить таймер?")
